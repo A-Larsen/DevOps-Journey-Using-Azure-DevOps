a9d78aafe7f5dc6a8f9f3812a23ce2cad270c603
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-12-21 17:34:19 -0500

    first commit | changed initial setup scripts

--- labs/1-Initial-Setup/scripts/create-terraform-storage.sh
+++ labs/1-Initial-Setup/scripts/create-terraform-storage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-RESOURCE_GROUP_NAME="devops-journey-rg"
-STORAGE_ACCOUNT_NAME="devopsjourneyazuredevops"
+RESOURCE_GROUP_NAME="DevOps"
+STORAGE_ACCOUNT_NAME="sahwwqadevops"
 
 # Create Resource Group
 az group create -l uksouth -n $RESOURCE_GROUP_NAME
